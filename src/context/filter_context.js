f0e32c718a8a5f0e0168596e24ba6f5452657389
namphuong2217 <namphuong2217@gmail.com>
2021-02-12 12:24:00 +0100

    Filters Pages

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useContext, useReducer } from 'react'
-import reducer from '../reducers/filter_reducer'
+import React, { useEffect, useContext, useReducer } from "react";
+import reducer from "../reducers/filter_reducer";
 import {
   LOAD_PRODUCTS,
   SET_GRIDVIEW,
@@ -9,21 +9,99 @@ import {
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
-} from '../actions'
-import { useProductsContext } from './products_context'
+} from "../actions";
+import { useProductsContext } from "./products_context";
 
-const initialState = {}
+const initialState = {
+  filtered_products: [],
+  all_products: [],
+  grid_view: true,
+  sort: "lowest-price", //default value
+  filters: {
+    text: "",
+    company: "all",
+    category: "all",
+    color: "all",
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+    shipping: false,
+  },
+};
 
-const FilterContext = React.createContext()
+const FilterContext = React.createContext();
 
 export const FilterProvider = ({ children }) => {
+  const { products } = useProductsContext(); // use data from other context
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  //filter can only receive data by 1: React Components or 2: Inside Context.Provider
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products });
+  }, [products]);
+
+  useEffect(() => {
+     // use within this context and state.filter change only. For actually filter product when state.filters changes
+    dispatch({ type: FILTER_PRODUCTS });
+    dispatch({ type: SORT_PRODUCTS });
+  }, [products, state.sort, state.filters]);
+
+  const setGridView = () => {
+    dispatch({ type: SET_GRIDVIEW });
+  };
+
+  const setListView = () => {
+    dispatch({ type: SET_LISTVIEW });
+  };
+
+  const updateSort = (e) => {
+    const name = e.target.name; //retrieve select mit id sort tag
+    const value = e.target.value; // <option value="lowest-price">lowest price</option>
+    // console.log(name, value);
+    dispatch({ type: UPDATE_SORT, payload: value });
+  };
+
+  //when click on filter on the left
+  // use on Filter React Component
+  const updateFilters = (e) => {
+    // <input type="text" name="text"></input>;
+    let name = e.target.name;
+    let value = e.target.value; //cannot access the text inside of a button
+    if (name === "category") {
+      value = e.target.textContent;
+    }
+    if (name === "color") {
+      value = e.target.dataset.color; // data-color={c}
+    }
+    if (name === "price") {
+      value = Number(value);
+    }
+    if (name === "shipping") {
+      value = e.target.checked;
+    }
+    dispatch({ type: UPDATE_FILTERS, payload: { name, value } });
+  };
+
+  const clearFilters = () => {
+    dispatch({ type: CLEAR_FILTERS });
+  };
+
   return (
-    <FilterContext.Provider value='filter context'>
+    <FilterContext.Provider
+      value={{
+        ...state,
+        setGridView,
+        setListView,
+        updateSort,
+        updateFilters,
+        clearFilters,
+      }}
+    >
       {children}
     </FilterContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useFilterContext = () => {
-  return useContext(FilterContext)
-}
+  return useContext(FilterContext);
+};
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -7,11 +7,124 @@ import {
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
-} from '../actions'
+} from "../actions";
+
+//filter can only receive data by 1: React Components or 2: Inside Context.Provider
 
 const filter_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === LOAD_PRODUCTS) {
+    //start new section
+    let maxPrice = action.payload.map((p) => p.price);
+    maxPrice = Math.max(...maxPrice);
+    // console.log(maxPrice)
+    //end new section
 
-export default filter_reducer
+    // must copy ...action.payload. Otherwise
+    //  all_products will point to a memory reference where the data has changed/filtered
+    //copy the value. Normally React will point to memory location of payload
+    return {
+      ...state,
+      all_products: [...action.payload],
+      filtered_products: [...action.payload],
+      filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
+    };
+  }
+
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, grid_view: true };
+  }
+
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, grid_view: false };
+  }
+
+  if (action.type === UPDATE_SORT) {
+    return { ...state, sort: action.payload };
+  }
+
+  // this take place via useEffect, via change of UPDATE_SORT
+  if (action.type === SORT_PRODUCTS) {
+    const { sort, filtered_products } = state;
+    let tempProducts = [...filtered_products];
+    if (sort === "lowest-price") {
+      tempProducts = tempProducts.sort((a, b) => a.price - b.price);
+    }
+    if (sort === "highest-price") {
+      tempProducts = tempProducts.sort((a, b) => b.price - a.price);
+    }
+    if (sort === "name-a") {
+      tempProducts = tempProducts.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+    }
+    if (sort === "name-z") {
+      tempProducts = tempProducts.sort((a, b) => {
+        return b.name.localeCompare(a.name);
+      });
+    }
+    return { ...state, filtered_products: tempProducts };
+  }
+
+  // this will lead to FILTER_PRODUCTS
+  if (action.type === UPDATE_FILTERS) {
+    const { name, value } = action.payload;
+    //[name] access the variable dynamically
+    return { ...state, filters: { ...state.filters, [name]: value } };
+  }
+
+  // kick off on 2-level
+  if (action.type === FILTER_PRODUCTS) {
+    const { all_products } = state;
+    const { text, category, company, color, price, shipping } = state.filters;
+
+    //filter the original data
+    let temp = [...all_products];
+
+    if (text) {
+      temp = temp.filter((product) => {
+        return product.name.toLowerCase().startsWith(text);
+      });
+    }
+
+    if (category !== "all") {
+      temp = temp.filter((product) => product.category === category);
+    }
+
+    if (company !== "all") {
+      temp = temp.filter((product) => product.company === company);
+    }
+
+    if (color !== "all") {
+      temp = temp.filter((product) => {
+        return product.colors.find((c) => c === color);
+      });
+    }
+
+    temp = temp.filter((product)=> product.price < price)
+
+    if (shipping) {
+      temp = temp.filter((product)=>product.shipping === true);
+    }
+    return { ...state, filtered_products: temp };
+  }
+
+  if (action.type === CLEAR_FILTERS) {
+    return {
+      ...state,
+      filters: {
+        ...state.filters,
+        text: "",
+        company: "all",
+        category: "all",
+        color: "all",
+        price: state.filters.max_price,
+        shipping: false,
+      },
+    };
+  }
+
+  return state;
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default filter_reducer;
